9b473a70432c2e57f3067de40e33b6706990aa46
max keller <max.keller427@gmail.com>
2019-05-08 21:31:57 -0600

    completed delete and put, needs one bug fix

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ server.get("/api/users/:id", (req, res) => {
 
   db.findById(id)
     .then(user => {
-      console.log(user);
-      if (id) {
+      if (user.id !== null) {
         res.json(user);
       } else {
         res
@@ -59,6 +58,49 @@ server.get("/api/users/:id", (req, res) => {
         .status(500)
         .json({ error: "The user information could not be retrieved." })
     );
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err =>
+      res.status(400).json({ error: "The user could not be removed" })
+    );
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.update(id, changes)
+    .then(user => {
+      if (user.name || user.bio === null) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user." });
+      } else if (user.id === null) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
 });
 
 server.listen(5000, () => console.log("running"));
